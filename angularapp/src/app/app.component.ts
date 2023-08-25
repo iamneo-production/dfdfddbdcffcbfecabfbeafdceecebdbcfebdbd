44193db1cb877f4276ac38e6ac9df0f394c7ef0f
Iamneo <techadmin@iamneo.ai>
2023-08-25 08:53:22 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { ConvertPipe } from './convert.pipe';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'currency_convertor';
