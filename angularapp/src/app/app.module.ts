bca74fabce24eef9d1cc5f0cbf54457ea4c19535
Iamneo <techadmin@iamneo.ai>
2023-08-25 06:23:09 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ConvertPipe } from './convert.pipe';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class AppModule { }
+export class AppComponent {
+  title = 'currency_convertor';
+  inputForm: any = new FormGroup({
+    fromCurrency: new FormControl({ value: '', disabled: false }),
+    toCurrency: new FormControl({ value: '', disabled: false }),
+    amount: new FormControl({ value: '', disabled: false }),
+    submitButton: new FormControl({ value: '', disabled: false }),
+    resValue: new FormControl({ value: '', disabled: false })
+  })
+  currency = [{ "id": "USD", "value": 1.126735 },
+  { "id": "GBP", "value": 0.876893 },
+  { "id": "INR", "value": 79.677056 }
+  ]
+  ngOninit() {
+
+  }
+  submitButton() {
+    let val = new ConvertPipe().transform(this.inputForm.value.fromCurrency,this.inputForm.value.toCurrency,this.inputForm.value.amount )
+    val = String(val) + '.00'
+    this.inputForm.value.resValue = val
+  }
+}
